d1c2f2dce8126407953d0034af859a42660ccde9
Seondong Pyo <psd1390@gmail.com>
2020-12-20 04:18:25 +0900

    Welcome 페이지 및 컨트롤러 작성

---
+++ src/test/java/com/example/controller/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc; // MockMvc 주입
+
+    @Test
+    public void testHomepage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/")) // HTTP GET '/' 요청
+                .andExpect(MockMvcResultMatchers.status().isOk()) // HTTP 200
+                .andExpect(MockMvcResultMatchers.view().name("home")) // 뷰의 이름은 home
+                .andExpect(MockMvcResultMatchers.content().string(
+                        Matchers.containsString("Welcome to Taco Cloud") // 뷰 콘텐츠에 'Welcome To Taco Cloud' 포함
+                ));
+    }
+}
