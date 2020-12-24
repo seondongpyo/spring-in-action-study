812de3b74413021096c6a653fade740ee002db0a
Seondong Pyo <psd1390@gmail.com>
2020-12-24 23:06:34 +0900

    뷰 컨트롤러 작성

---
+++ src/main/java/com/example/web/WebConfig.java
@@ -0,0 +1,14 @@
+package com.example.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer { // 뷰 컨트롤러
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+}
