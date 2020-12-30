ce6642b266e54c710490d5871448cb94479fcfbe
seondongpyo <psd1390@gmail.com>
2020-12-31 00:59:25 +0900

    주문 양식 변경 및 컨버터 추가

---
+++ src/main/java/com/example/web/IngredientByIdConverter.java
@@ -0,0 +1,19 @@
+package com.example.web;
+
+import com.example.domain.Ingredient;
+import com.example.repository.IngredientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    private final IngredientRepository ingredientRepository;
+
+    @Override
+    public Ingredient convert(String id) {
+        return ingredientRepository.findById(id);
+    }
+}
