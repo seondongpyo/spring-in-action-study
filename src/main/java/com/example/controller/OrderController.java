bf7c0d99b35945a7da15718b60363aff464e52ee
seondongpyo <psd1390@gmail.com>
2020-12-31 00:53:40 +0900

    주문 처리 로직 추가

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -1,35 +1,44 @@
 package com.example.controller;
 
 import com.example.domain.Order;
+import com.example.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller
+@SessionAttributes("order")
 @RequestMapping("/orders")
 public class OrderController {
 
-    @GetMapping("/current")
-    public String orderForm(Model model) {
-        model.addAttribute("order", new Order());
+    private final OrderRepository orderRepository;
 
+    @GetMapping("/current")
+    public String orderForm() {
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors) {
+    public String processOrder(@Valid Order order,
+                               Errors errors,
+                               SessionStatus sessionStatus) {
+
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
-        log.info("Order submitted: " + order);
+        orderRepository.save(order);
+        sessionStatus.setComplete();
 
         return "redirect:/";
     }
