b415c1437e6f27983741d8602b92e43c54a163bc
seondongpyo <psd1390@gmail.com>
2021-01-09 08:46:05 +0900

    JDBC 기반의 사용자 스토어 추가

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -1,14 +1,21 @@
 package com.example.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,6 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 인메모리 기반
+        /*
         auth.inMemoryAuthentication()
                 .withUser("user1")
                 .password("{noop}password1")
@@ -31,5 +40,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user2")
                 .password("{noop}password2")
                 .authorities("ROLE_USER");
+         */
+
+        // JDBC 기반
+        auth.jdbcAuthentication()
+                .dataSource(dataSource);
     }
 }
