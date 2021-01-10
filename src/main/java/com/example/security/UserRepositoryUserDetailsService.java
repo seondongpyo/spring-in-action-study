56c2afb929c19539a4cd39142f662d190752e412
seondongpyo <psd1390@gmail.com>
2021-01-10 09:14:01 +0900

    사용자 명세 repo, service 생성

---
+++ src/main/java/com/example/security/UserRepositoryUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.security;
+
+import com.example.domain.User;
+import com.example.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user != null) {
+            return user;
+        }
+
+        throw new UsernameNotFoundException("User '" + username + "' not found");
+    }
+}
